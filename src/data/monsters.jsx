aad1474106e9a30784f8d3bc3c6ad78f6799cc21
sjeremich23 <51142646+sjeremich23@users.noreply.github.com>
2019-11-12 14:30:19 -0700

    First Commit

---
+++ src/data/monsters.jsx
@@ -0,0 +1,20 @@
+const monsters = [
+  {
+    name: "Frankenstein",
+    id: "asc1"
+  },
+  {
+    name: "Dracula",
+    id: "asr2"
+  },
+  {
+    name: "Zombie",
+    id: "as1w"
+  },
+  {
+    name: "Banshee",
+    id: "as1z"
+  }
+];
+
+export default monsters;
